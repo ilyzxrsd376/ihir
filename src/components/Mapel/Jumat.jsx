1b67ab52e95d4871c559c3f68fb9c6f3c3ac0342
ilyzxrsd376 <ilyzxrsd@gmail.com>
2024-08-31 22:43:49 +0700

    Jumat.jsx

--- src/components/Mapel/Jumat.jsx
+++ src/components/Mapel/Jumat.jsx
@@ -40,8 +40,8 @@ const Jumat = () => {
 				className="border-b-2 flex justify-between py-[0.50rem] w-72 px-3 opacity-60"
 				data-aos="fade-up"
 				data-aos-duration="600">
-				<div className="w-[50%] text-base  font-medium">Sholat Jumat</div>
-				<div className="flex justify-center items-center text-sm">11.50-12.50</div>
+				<div className="w-[50%] text-base  font-medium">Jumat Bersih</div>
+				<div className="flex justify-center items-center text-sm">12.50-14.00</div>
 			</div>
 			<div
 				className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60"
